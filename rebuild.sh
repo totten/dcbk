3cd9fc71b94cbb94f6a5516fbb47849f1fd293c3
Tim Otten <totten@civicrm.org>
2017-03-10 17:35:34 -0800

    rebuild.sh - Make it easier to skim sections of output

--- rebuild.sh
+++ rebuild.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+set -e
 
 # This script is merely a little sugar to automate the build, start
 # and test cycle.
 
-echo "Rebuilding the image"
+echo "[Rebuilding the image]"
 docker build -t dcbk:v1 .
 
-echo "Cleaning up old containers"
-docker rm -f buildkit
-echo "Making folders for the mounted volumes"
+echo "[Cleaning up old containers]"
+docker rm -f buildkit && 'echo Deleted' || echo 'Nothing to delete'
+echo "[Making folders for the mounted volumes]"
 mkdir -p ampuser build
-echo "Making a container based on the image"
+echo "[Making a container based on the image]"
 docker create  -p 2222:2222  -p 8001:8001  -p 8000:80  -v $(pwd)/ampuser:/home/ampuser -v $(pwd)/build:/opt/buildkit/build --name buildkit dcbk:v1
-echo "starting the container"
+echo "[Starting the container]"
 docker start buildkit
 
-echo "build complete!"
+echo "[Build complete!]"
 echo "
 You can open a bash shell with this command:
 
